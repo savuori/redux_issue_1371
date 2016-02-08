ee35fa1dfe642b0e329ffb9e948b29c4d07a6a50
Sampo Vuori <sampo@kepit.fi>
2016-02-08 17:32:41 +0200

    initial commit

---
+++ web/static/js/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+
+const rootEl = document.getElementById('root')
+
+function render() {
+  ReactDOM.render(
+    <div>blarg</div>,
+    rootEl  
+  )
+}
+
+render()
